8f12c08baf83ca1c532e65d726c4bd33cd74e38f
Anurag0509tripathi <167770455+Anurag0509tripathi@users.noreply.github.com>
2026-05-03 11:34:49 +0530

    Add Python operators practice

---
+++ python-basics/operators_practice.py
@@ -0,0 +1,16 @@
+# Python operators practice
+
+a = 20
+b = 6
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+print("Is a greater than b?", a > b)
+print("Is a equal to b?", a == b)
+print("Is a not equal to b?", a != b)
