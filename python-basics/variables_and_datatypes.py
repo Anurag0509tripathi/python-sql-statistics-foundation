6f696950c18cfffe88a223b12ac8ebab3446a559
Anurag0509tripathi <167770455+Anurag0509tripathi@users.noreply.github.com>
2026-05-03 11:34:00 +0530

    Add Python variables and data types practice

---
+++ python-basics/variables_and_datatypes.py
@@ -0,0 +1,16 @@
+# Python variables and data types practice
+
+name = "Anurag"
+age = 21
+height = 5.8
+is_learning_data_science = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Data Science:", is_learning_data_science)
+
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_learning_data_science:", type(is_learning_data_science))
